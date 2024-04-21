477627d0b84f976163c4064103b88e9a83c6417e
philip713 <105677806+philip713@users.noreply.github.com>
2024-04-20 21:52:06 -0600

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Philip Janzel Paradeza on 2024-04-20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
